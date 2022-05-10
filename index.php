a97b9a54a247880407585ad2f20dbe865205f03f
sumitgangwar-cedcommerce <104122327+sumitgangwar-cedcommerce@users.noreply.github.com>
2022-05-10 11:57:43 +0530

    Write a PHP program using nested for loop that creates a chess board.

--- index.php
+++ index.php
@@ -7,18 +7,26 @@
     <title>Document</title>
 </head>
 <body>
-    <form name="form" action="index.php" method="get">
-        <input type="text"  name = "number" placeholder="Enter number">
-        <input type="submit">
-    </form>
     <?php
-        $number =  $_GET['number']; 
-        $fact = 1;
-        for($number;$number>1;$number--){
-            $fact*=$number;
+    echo "<table style='width=400px ; border-collapse = collapse;margin:0px;padding:0px' >";
+    $t = 0;
+       for($i=0;$i<9;$i++){
+           echo "<tr style='border: solid;margin:0px;padding:0px'>";
+    
+        for($j=0;$j<9;$j++){
+           if($t==0){
+            echo "<td  style='height=30px ; background-color: black;border: solid; margin:0px; padding:0px'>sfcf</td>";
+            $t = 1;
+           }
+           else{
+            echo "<td style='height=30px ; background-color = white ; color:  white;border:solid;margin:0px;padding:0px'>rrr</td>";
+            $t = 0;
+           } 
+           
         }
-        echo $fact;
-        
+            echo "</tr>";    
+       }
+    echo "</table>"; 
     ?>
 </body>
 </html>

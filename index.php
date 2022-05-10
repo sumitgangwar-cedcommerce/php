d083efcd6e1957d57d9a05d236b1a40ba8ca1757
sumitgangwar-cedcommerce <104122327+sumitgangwar-cedcommerce@users.noreply.github.com>
2022-05-10 11:17:58 +0530

    Write a program to check student grade based on the marks using if-else

--- index.php
+++ index.php
@@ -7,9 +7,16 @@
     <title>Document</title>
 </head>
 <body>
+    <form name="form" action="index.php" method="get">
+        <input type="text" id="marks" name = "marks" placeholder="Enter marks">
+        <input type="submit">
+    </form>
     <?php
-        $a = "Print"; $b = "PHP";
-        echo $a." two ".$b." variable in singe echo";
+        $marks =  $_GET['marks']; 
+        if($marks>=60)  echo "First Division";
+        elseif($marks>=45 && $marks<=59)  echo "Second Division";
+        elseif($marks>=33 && $marks<=44)  echo "Third Division";
+        else echo "Fail";
     ?>
 </body>
 </html>

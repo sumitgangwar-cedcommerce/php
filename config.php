ad7f2b0704d24f0fa32f37c4cf460d33d93dd115
sumitgangwar-cedcommerce <104122327+sumitgangwar-cedcommerce@users.noreply.github.com>
2022-05-12 18:22:18 +0530

    cart session and cookie

---
+++ config.php
@@ -0,0 +1,33 @@
+<?php
+    $tag_line = "This is an Online Store";
+    $creater = "Sumit";
+    $products  =array(
+		array(
+			"image"=>"images/football.png",
+			"name"=>"Product 101",
+			"price"=>"$150.00"
+		),
+		array(
+			"image"=>"images/basketball.png",
+			"name"=>"Product 102",
+			"price"=>"$120.00"
+		),
+		array(
+			"image"=>"images/table-tennis.png",
+			"name"=>"Product 103",
+			"price"=>"$90.00"
+		),
+		array(
+			"image"=>"images/tennis.png",
+			"name"=>"Product 104",
+			"price"=>"$110.00"
+		),
+		array(
+			"image"=>"images/soccer.png",
+			"name"=>"Product 105",
+			"price"=>"$80.00"
+		)
+		);        
+?>
+
+
